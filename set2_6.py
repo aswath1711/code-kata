d3f4364669c9efdd5f924d70d7ccd01df78b3ead
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-05-28 10:24:54 +0530

    Update set2_6.py

--- set2_6.py
+++ set2_6.py
@@ -1,6 +1,6 @@
-lower,upper=[int(x) for x in input().split()]
-#print(lower,upper)
-for num in range(lower,upper + 1):
+l,u=[int(x) for x in input().split()]
+#print(l,u)
+for num in range(l,u + 1):
    if num > 1:
        for i in range(2,num):
            if (num % i) == 0:
