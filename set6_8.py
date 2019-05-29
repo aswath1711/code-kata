584b40c9e4fbbe35ca84f6afee62bda8400e695d
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-05-30 00:02:41 +0530

    Add files via upload

---
+++ set6_8.py
@@ -0,0 +1,10 @@
+n,k=[int(x) for x in input().split()]
+li=list(map(int,input().split()))
+count=0
+for i in range(n):
+    if(li[i]==k):
+        count==1
+if(count==1):
+    print("yes")
+else:
+    print("no")
