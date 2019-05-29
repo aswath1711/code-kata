ae683cbb22415d9168cd03fd50b16518ab87fe3a
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-05-29 23:51:17 +0530

    Add files via upload

---
+++ set6_6.py
@@ -0,0 +1,12 @@
+n=input()
+count1=0
+count2=0
+for i in n:
+    if(i.isnumeric()):
+        count1==1
+    if(i.isalphabet()):
+        count2==1
+if(count1==1 and count2==1):
+    print("Yes")
+else:
+    print("No")
