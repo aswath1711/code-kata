3b14bff1e7973374f1d461f8fdfd4644e9fbd0c2
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-05-30 11:13:13 +0530

    Update set8_6.py

--- set8_6.py
+++ set8_6.py
@@ -1,10 +1,10 @@
 number=int(input())
 if number > 1: 
-    for iter in range(2, number//2): 
-       if(number % iter) == 0: 
-           print("yes") 
-           break
+	for iter in range(2, number//2): 
+	       if (number % iter) == 0: 
+	           print("yes") 
+	           break
 	else: 
-       print("no") 
+	       print("no") 
 else: 
-    print("yes")
+	print("yes") 
