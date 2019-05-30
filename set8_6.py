962480e0e791cb20d098fc0c543ac85ab14a42bf
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-05-30 11:10:38 +0530

    Add files via upload

---
+++ set8_6.py
@@ -0,0 +1,10 @@
+number=int(input())
+if number > 1: 
+    for iter in range(2, number//2): 
+       if(number % iter) == 0: 
+           print("yes") 
+           break
+	else: 
+       print("no") 
+else: 
+    print("yes")
